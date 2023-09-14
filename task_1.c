e73d07915725f98bd68a59eee804dfcc13e31cec
SodimuDemilade <demisodimu@gmail.com>
2023-09-14 05:46:20 -0700

    simple shell

---
+++ task_1.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+/**
+ *
+ *
+ *
+ */
+
+int main()
+{
+	while (1)
+	{
+	char *buffer, *argv[] = {NULL};
+	size_t buff_size = 1024, c;
+	int stat;
+
+	buffer = malloc(sizeof(char) * buff_size);
+	if (buffer == NULL)
+	{
+		perror("Error allocating memory:");
+		return (1);
+	}
+	write(STDOUT_FILENO, "#command$ ", 10);
+	c = getline(&buffer, &buff_size, stdin);
+	if (c == -1)
+	{
+		if (feof(stdin))
+		{
+			free(buffer);
+			break;
+		}
+		else
+		{
+			perror("Error reading input:");
+			free(buffer);
+			return(1);
+		}
+	}
+	buffer[c - 1] = '\0';
+	pid_t child = fork();
+	if (child == -1)
+	{
+		perror("Error:");
+		free(buffer);
+		return (1);
+	}
+	if (child == 0)
+	{
+		argv[0] = buffer;
+		argv[1] = NULL;
+		if (execve(buffer, argv, NULL) == -1)
+		{
+			perror("Error:");
+			free(buffer);
+			exit(1);
+		}
+	}
+	else
+	{
+		waitpid(child, &stat, 0);
+	}
+	free(buffer);
+	}
+	write(STDOUT_FILENO, "\n", 2);
+	return (0);
+}
