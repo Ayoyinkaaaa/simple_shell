737ed5c48219e08c95d2e41f04491b5d1fcc8be5
Ayoyinkaaaa <Egunjobikao@gmail.com>
2023-09-20 06:15:37 -0700

    tasks

--- shell.h
+++ shell.h
@@ -16,9 +16,13 @@ extern char **environ;
 
 void parse_arguments(char *input, char *argv[]);
 int main (void);
-bool is_command_in_path(void);
 void exec_comm(char *buffer, char *argv[]);
 int command_exits(const char *buffer);
 void env_func(void);
 ssize_t get_line(char *buf, size_t nbytes);
+int iscdCommand(char *command);
+int cdCommand(char *directory);
+char *get_location(char *command);
+char *search_in_directory(const char *directory, const char *command);
+char *search_in_path(const char *path, const char *command);
 #endif
